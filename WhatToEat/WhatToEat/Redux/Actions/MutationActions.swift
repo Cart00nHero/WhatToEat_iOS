0f86adaf3bf52ebd7243dceef8826f4288cda002
Cart00nHero8 <Cart00nHero8@gmail.com>
2020-06-08 23:36:30 +0800

    Add Location Query

--- WhatToEat/WhatToEat/Redux/Actions/MutationActions.swift
+++ WhatToEat/WhatToEat/Redux/Actions/MutationActions.swift
@@ -1,8 +1,8 @@
 //
-//  ApiActions.swift
+//  MutationActions.swift
 //  WhatToEat
 //
-//  Created by YuCheng on 2020/5/31.
+//  Created by YuCheng on 2020/6/8.
 //  Copyright © 2020 YuCheng. All rights reserved.
 //
 
@@ -22,13 +22,12 @@ struct CreateLocationAction: Action, ApiActionProtocol {
 func createLocationAction(newLoc: GQAddress) -> CreateLocationAction {
     var action = CreateLocationAction()
     let service = ApolloService.shared.apollo
-    let createLocationMutation = CreateLocationMutation(ownerType: newLoc.ownerType, completeInfo: newLoc.completeInfo, fullInfo: newLoc.fullInfo, postalCode: newLoc.postalCode, nation: newLoc.nation, isoNationCode: newLoc.isoNationCode, locality: newLoc.locality, subLocality: newLoc.subLocality, administrativeArea: newLoc.administrativeArea, subAdministrativeArea: newLoc.subAdministrativeArea, thoroughfare: newLoc.thoroughfare, subThoroughfare: newLoc.subThoroughfare, floor: newLoc.floor, latitude: String(format:"%f", newLoc.latitude), longitude: String(format:"%f", newLoc.longitude), annotation: newLoc.annotation, shopBranch: newLoc.shopBranch)
-    service.perform(mutation: createLocationMutation) { result in
+    let mutation = CreateLocationMutation(ownerType: newLoc.ownerType, completeInfo: newLoc.completeInfo, fullInfo: newLoc.fullInfo, postalCode: newLoc.postalCode, nation: newLoc.nation, isoNationCode: newLoc.isoNationCode, locality: newLoc.locality, subLocality: newLoc.subLocality, administrativeArea: newLoc.administrativeArea, subAdministrativeArea: newLoc.subAdministrativeArea, thoroughfare: newLoc.thoroughfare, subThoroughfare: newLoc.subThoroughfare, floor: newLoc.floor, latitude: String(format:"%f", newLoc.latitude), longitude: String(format:"%f", newLoc.longitude), annotation: newLoc.annotation, shopBranch: newLoc.shopBranch)
+    service.perform(mutation: mutation) { result in
         switch result {
             
         case .success(let graphQLResult):
             if graphQLResult.errors != nil {
-                print("幹咧！！")
                 print(graphQLResult.errors?.description ?? "")
                 action.status = .Failed
             }
---
+++ WhatToEat/WhatToEat/Redux/Actions/QueryActions.swift
@@ -0,0 +1,37 @@
+//
+//  QueryActions.swift
+//  WhatToEat
+//
+//  Created by YuCheng on 2020/6/8.
+//  Copyright © 2020 YuCheng. All rights reserved.
+//
+
+import UIKit
+import ReSwift
+
+struct LocationsDynamicQueryAction: Action, ApiActionProtocol {
+    var status: ApiActionStatus = .Started
+}
+func locationsDynamicQueryAction(whereCMD: InputAddress) -> LocationsDynamicQueryAction {
+    var action = LocationsDynamicQueryAction()
+    let service = ApolloService.shared.apollo
+    let query = LocationsDynamicQueryQuery(whereAnd: whereCMD)
+    service.fetch(query: query) { result in
+        switch result {
+            
+        case .success(let graphQLResult):
+            if graphQLResult.errors != nil {
+                print(graphQLResult.errors?.description ?? "")
+                action.status = .Failed
+            }
+            if (graphQLResult.data?.locationsDynamicQuery) != nil {
+                action.status = .Success
+            }
+        case .failure(let error):
+            print(error.localizedDescription)
+            action.status = .Failed
+        }
+    }
+    
+    return action
+}
