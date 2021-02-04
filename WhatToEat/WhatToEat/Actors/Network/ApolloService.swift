f70649fadf6c621876c471330e3c32d866ba3b9b
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-02-04 21:20:45 +0800

    Update

--- WhatToEat/WhatToEat/Actors/Network/ApolloService.swift
+++ WhatToEat/WhatToEat/Actors/Network/ApolloService.swift
@@ -11,7 +11,7 @@ import Apollo
 
 class ApolloService: NSObject {
     static let shared = ApolloService()
-    private(set) lazy var apollo = ApolloClient(url: URL(string: "http://3.112.246.100:8080/gql/api")!)
+    private(set) lazy var apollo = ApolloClient(url: URL(string: "http://3.112.151.31:8080/gql/api")!)
 }
 
 func clearApolloServiceCache() {
