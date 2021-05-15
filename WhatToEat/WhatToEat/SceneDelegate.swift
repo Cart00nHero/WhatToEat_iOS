07a250cb72f5ea64f931358a3bd8c25389908eed
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-05-15 19:41:56 +0800

    Auto stash before merge of "develop" and "origin/develop"

--- WhatToEat/WhatToEat/SceneDelegate.swift
+++ WhatToEat/WhatToEat/SceneDelegate.swift
@@ -41,7 +41,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        Flynn.startup()
         appStore.dispatch(SceneWillEnterForegroundAction())
     }
 
@@ -50,7 +49,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
         appStore.dispatch(SceneDidEnterBackgroundAction())
-        Flynn.shutdown()
     }
 
 
